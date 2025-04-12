d60f92014bbc1e449eeb64c6af4bbd354f02629b
Laviesss <laviesamuelferrieratrocard@gmail.com>
2025-04-12 12:22:40 -0400

    Changes

---
+++ src/main/java/com/laviesss/xaerodisabledradarfixer/XaeroDisabledRadarFixer.java
@@ -0,0 +1,7 @@
+package com.laviesss.xaerodisabledradarfixer;
+
+public class XaeroDisabledRadarFixer {
+    public static void init() {
+        System.out.println("XaeroDisabledRadarFixer loaded!");
+    }
+}
