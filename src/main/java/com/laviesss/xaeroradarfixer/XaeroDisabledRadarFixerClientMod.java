fc5237fd8ec3d32178b264a145deb065a247c8b1
Laviesss <laviesamuelferrieratrocard@gmail.com>
2025-04-16 19:54:41 -0400

    Changes/Fixes

--- src/main/java/com/laviesss/xaeroradarfixer/XaeroDisabledRadarFixerClientMod.java
+++ src/main/java/com/laviesss/xaeroradarfixer/XaeroDisabledRadarFixerClientMod.java
@@ -3,6 +3,8 @@ package com.laviesss.xaerodisabledradarfixer;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import com.laviesss.xaeroradarfixer.XaeroDisabledRadarFixerMixin;
+
 
 public class XaeroDisabledRadarFixerClientMod implements ClientModInitializer {
 
