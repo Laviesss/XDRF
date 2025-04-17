ed2d67261648cdec9d122378196692d6cb5403e4
Laviesss <laviesamuelferrieratrocard@gmail.com>
2025-04-17 14:12:26 -0400

    Changes/Fixes

--- src/main/java/com/laviesss/xaeroradarfixer/mixin/XaeroDisabledRadarFixerMixin.java
+++ src/main/java/com/laviesss/xaeroradarfixer/mixin/XaeroDisabledRadarFixerMixin.java
@@ -1,11 +1,8 @@
 package com.laviesss.xaeroradarfixer.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.text.Text;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,14 +13,18 @@ public class XaeroDisabledRadarFixerMixin {
 
     @Inject(method = "onGameMessage", at = @At("HEAD"), cancellable = true)
     private void onGameMessage(GameMessageS2CPacket packet, CallbackInfo ci) {
+        // only block when in a world
+        if (net.minecraft.client.MinecraftClient.getInstance().world == null) return;
+
         Text message = packet.content();
         String content = message.getString();
 
-        // Check for specific patterns in the message content
-        if (content.contains("§ƒ§ə§i§r§×§a§e§Ã§o") ||
+        if ( content.contains("§ƒ§ə§i§r§×§a§e§Ã§o") ||
                 content.contains("§x§a§e§Ã§o§w§m§§§§§§r§i§§ƒ§ä§i§r") ||
-                content.contains("§ƒ§ə§i§r§×§a§e§Ã§o §x§a§e§Ã§o§w§m§§§§§§r§i§§ƒ§ä§i§r")) {
-            ci.cancel(); // Block the message if it matches any of the patterns
+                content.contains("§ƒ§ə§i§r§×§a§e§Ã§o §x§a§e§Ã§o§w§m§§§§§§r§i§§ƒ§ä§i§r") ||
+                content.contains("§f§a§i§r§x§a§e§r§o §x§a§e§r§o§w§m§n§e§t§h§e§r§i§s§f§a§i§r")
+        ) {
+            ci.cancel();
         }
     }
 }
